538953292dea5bfaa9effd378a004f0c32c7ce78
Roman Korzh <roman.korzh1989@gmail.com>
2022-11-18 10:49:22 +0200

    first start commit

---
+++ src/main/java/lesson_one/Main.java
@@ -0,0 +1,8 @@
+package lesson_one;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This is the first lesson");
+    }
+}
