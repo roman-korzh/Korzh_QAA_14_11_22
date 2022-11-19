3e8eba0021b0296a91af151d5bee1bb5d1c28785
Roman Korzh <roman.korzh1989@gmail.com>
2022-11-19 21:00:13 +0200

    test string added

--- src/main/java/lesson_one/Main.java
+++ src/main/java/lesson_one/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("This is the first lesson");
+        System.out.println("Another test string");
     }
 }
