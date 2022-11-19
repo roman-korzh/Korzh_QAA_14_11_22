26b32ae677dbe6f775dd39eba4f4155f3800bca8
roman-korzh <roman.korzh1989@gmail.com>
2022-11-20 00:43:04 +0200

    str for testing

--- src/main/java/lesson_one/Main.java
+++ src/main/java/lesson_one/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("This is the first lesson");
         System.out.println("Another test string");
+        System.out.println("String for testing");
     }
 }
